9bceae782d0a79905e4aca3d528c58926a50d54f
tommydemarco <tommaso.demarco95gmail.com>
2020-12-15 00:02:32 +0100

    fixing imports issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Router, Switch, Route } from "react-router";
+import React from "react";
+import { Switch, Route } from "react-router";
 import TheHeader from "./components/TheHeader/TheHeader";
 import TheFooter from "./components/TheFooter/TheFooter";
 import SideContacts from "./components/SideContacts/SideContacts";
